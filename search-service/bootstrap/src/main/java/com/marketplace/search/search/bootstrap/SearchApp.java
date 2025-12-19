669bd52f86c8955a59e9b5828f609e3e366f9728
Pablo Santos <pablords@gmail.com>
2025-12-19 01:28:18 -0300

    feat: update launch configurations and add Search Service application with bootstrap class

---
+++ search-service/bootstrap/src/main/java/com/marketplace/search/search/bootstrap/SearchApp.java
@@ -0,0 +1,36 @@
+package com.marketplace.search.search.bootstrap;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Classe principal da aplicação Search Service
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.marketplace.search.search.domain",
+        "com.marketplace.search.search.application",
+        "com.marketplace.search.search.infrastructure",
+        "com.marketplace.search.search.interfaces"
+})
+@Slf4j
+public class SearchApp implements CommandLineRunner {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @Override
+    public void run(String... args) {
+        log.info("{} app is running", appName);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApp.class, args);
+    }
+}
+

3863969f609392e18a61f5a404eda1293afcbf9e
Pablo <pablo@idha.com.br>
2026-04-25 17:12:09 -0300

    refactor: clean up unused imports and validation annotations in ProductCommandController

--- catalog-service/interfaces/src/main/java/com/marketplace/search/catalog/interfaces/rest/commands/controllers/ProductCommandController.java
+++ catalog-service/interfaces/src/main/java/com/marketplace/search/catalog/interfaces/rest/commands/controllers/ProductCommandController.java
@@ -1,14 +1,11 @@
 package com.marketplace.search.catalog.interfaces.rest.commands.controllers;
 
 import java.net.URI;
-import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +16,7 @@ import com.marketplace.search.catalog.interfaces.rest.commands.mappers.ProductMa
 import com.marketplace.search.catalog.interfaces.rest.dtos.ProductDTO;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+
 
 /**
  * Endpoints de comando (escrita) relacionados a produtos.

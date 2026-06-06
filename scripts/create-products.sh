5072f777300d31aa5e276a402cd7badd906eb7d1
Pablo <pablo@idha.com.br>
2026-06-05 22:01:47 -0300

    chore: standardize structured JSON logging, disable console ANSI, and optimize JVM startup for services

--- scripts/create-products.sh
+++ scripts/create-products.sh
@@ -9,4 +9,4 @@ source .venv/bin/activate
 
 # pip install -r ./dataset-generate/requirements.txt
 
-python dataset-generate/data_gen.py --dataset-file ./dataset-generate/data/cache/amazon_products.csv --total 1000 --concurrent-workers 50 --api-url http://api.lab.com.br/api/v1
+python dataset-generate/data_gen.py --dataset-file ./dataset-generate/data/cache/amazon_products.csv --total 10000 --concurrent-workers 30 --api-url http://api.lab.com.br/api/v1
